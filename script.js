e097c6c823ae1d3dc6ea5cdc47623b9bcdc611e2
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-01-29 19:37:51 -0600

    Commented out all of my code in all files.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+
 // var queryCity = "api.openweathermap.org/data/2.5/weather?q={city name}";
 // var queryCity = "api.openweathermap.org/data/2.5/weather?q=" + var for user + "?api-Key=" + apikey;
 // var queryForecast = "pro.openweathermap.org/data/2.5/forecast/hourly?q={city name},{country code}";
@@ -8,65 +13,65 @@
 
 // var cityName = "Austin";
 
-$(document).ready(function (){
+// $(document).ready(function (){
 
-    function getWeather (cityName){
-        var apiKey = "aed52c01e7f5375831def9553ce0837d";
+//     function getWeather (cityName){
+//         var apiKey = "aed52c01e7f5375831def9553ce0837d";
     
-        // var cityName = "api.openweathermap.org/data/2.5/weather?q={city name}";
+//         // var cityName = "api.openweathermap.org/data/2.5/weather?q={city name}";
         
-        var queryCity = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-        // console.log(queryCity);
-        $.ajax({
-            url: queryCity,
-            method: "GET"
-        }). then (function(results){
+//         var queryCity = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+//         // console.log(queryCity);
+//         $.ajax({
+//             url: queryCity,
+//             method: "GET"
+//         }). then (function(results){
            
-            console.log(results)
-            var getTemp = results.main.temp;
-            var getHumidity = results.main.humidity;
-            var getWindspeed = results.wind.speed;
-            // var currentDate = moment(results.dt, "X").format("MM/DD/YYYY");
-            // var row = $("<div class='row>");
-            // var col = $("<div class='col-sm-12'>");
-            // var img = $("<img>");
-            // img.attr("src", "http://openweathermap.org/img/w" + results.weather[0].icon + ".png");
+//             console.log(results)
+//             var getTemp = results.main.temp;
+//             var getHumidity = results.main.humidity;
+//             var getWindspeed = results.wind.speed;
+//             // var currentDate = moment(results.dt, "X").format("MM/DD/YYYY");
+//             // var row = $("<div class='row>");
+//             // var col = $("<div class='col-sm-12'>");
+//             // var img = $("<img>");
+//             // img.attr("src", "http://openweathermap.org/img/w" + results.weather[0].icon + ".png");
 
         
-            $(".cardTemp").append(getTemp);
-            $(".cardTemp").append(getHumidity);
-            $(".cardTemp").append(getWindspeed);
+//             $(".temperature").append(getTemp);
+//             $(".humidity").append(getHumidity);
+//             $(".windspeed").append(getWindspeed);
     
-            var lon = results.coord.lon;
-            var lat = results.coord.lat;
-            var queryUV = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
-            $.ajax({
-                url: queryUV,
-                method: "GET"
-            }).then (function (uvResults){
-                console.log(uvResults)
-                var getUV = uvResults.value
-                $(".cardTemp").append(getUV)
+//             var lon = results.coord.lon;
+//             var lat = results.coord.lat;
+//             var queryUV = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
+//             $.ajax({
+//                 url: queryUV,
+//                 method: "GET"
+//             }).then (function (uvResults){
+//                 console.log(uvResults)
+//                 var getUV = uvResults.value
+//                 $(".cardTemp").append(getUV)
     
-            }).catch (err => console.log(err))
-        });
-        // var getTemp = results.main.temp;
-        // var getHumidity = results.main.humidity;
-        // var getWindspeed = results.wind.speed;
-        // var getUV = uvResults;
+//             }).catch (err => console.log(err))
+//         });
+//         // var getTemp = results.main.temp;
+//         // var getHumidity = results.main.humidity;
+//         // var getWindspeed = results.wind.speed;
+//         // var getUV = uvResults;
     
-        // $(cardTemp).append(getTemp);
-        // $(cardTemp).append(getHumidity);
-        // $(cardTemp).append(getWindspeed);
-        // $(cardTemp).append(getUV)
-    }
+//         // $(cardTemp).append(getTemp);
+//         // $(cardTemp).append(getHumidity);
+//         // $(cardTemp).append(getWindspeed);
+//         // $(cardTemp).append(getUV)
+//     }
     
-    $(".citySearch").on('click', function(){
-        var whichCity = $(".enterCity").val();
-        localStorage.setItem("specificCity", whichCity);
-        getWeather(whichCity);
+//     $(".citySearch").on('click', function(){
+//         var whichCity = $(".enterCity").val();
+//         localStorage.setItem("specificCity", whichCity);
+//         getWeather(whichCity);
     
-    });
+//     });
     
     // var queryCity = "https://openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
     
